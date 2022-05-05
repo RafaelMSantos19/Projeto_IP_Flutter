28133f5de04b99a683a260d4095db59e886a1fbc
Rafael Santos <rafarag40@gmail.com>
2022-05-05 10:35:12 -0300

    Back pegando

--- ip_project/lib/main.dart
+++ ip_project/lib/main.dart
@@ -32,11 +32,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final valor = TextEditingController();
-  int ip = 0  ;
+  
+  
+  final _formclasse = GlobalKey<FormState>();
 
-  void calcula(ip){
-    return ip;
+  double ip = 0.0;
+  
+  var divide;
+  var _valor ; 
+
+  void _calcula(){
+    setState(() {
+      if(ip >= 0 && ip <=126){
+       _valor = ' A';
+    }
+    if(ip >= 128 && ip <=191){
+       _valor = ' B';
+    }
+    if(ip >= 192 && ip <=223){
+       _valor = ' C';
+    }
+    if(ip >= 224 && ip <=239){
+       _valor = ' D';
+    }
+    if(ip >= 240 && ip <=255){
+       _valor = ' E';
+    }
+    if(ip == 127){
+      _valor =' LocalHost';
+    }
+    });
   }
 
   @override
@@ -47,20 +72,24 @@ class _MyHomePageState extends State<MyHomePage> {
         
         title: Text(widget.title),
       ),
-      body: Column(
+      body: Form(
+        key: _formclasse,
+        child: Column(
+          
             children:<Widget>[
-              Padding(padding:EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-
-              child: TextField(
-                      controller: valor,
-                      decoration: InputDecoration(
-                                  hintText: 'Digite o IP', ),
+              TextFormField(
+                      decoration: const InputDecoration( hintText: 'Digite o IP'),
+                      validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                  return 'Digite um valor!!';
+                                  }else{
+                                    divide= value.split('.');
+                                    ip = double.parse(divide[0]);
+                                  }
+                                  return null;
+                            },
                       ),
-                      
-              ),
-              Padding(padding:EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-              
-              child: TextButton(
+             ElevatedButton(
                   
                   style: TextButton.styleFrom(
                     primary: Colors.white,
@@ -68,21 +97,18 @@ class _MyHomePageState extends State<MyHomePage> {
                     
                   ),
                   onPressed: () { 
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(content: Text('Resposta: ' + valor.text));
+                        if (_formclasse.currentState!.validate()) {
+                            _calcula();
                         }
-                      );
                    },
                   child: Text('Calcular a Classe'),
               ),
+              //Text('Valor do IP e: $ip'),
+             Text('O IP pertence a Classe: $_valor ', 
+                              style: TextStyle(fontSize:20.0)),
               
-              ),
-              Padding(padding:EdgeInsets.symmetric(horizontal: 50, vertical: 10),
-                child: Text('O IP pertence a Classe: '+'$ip'),
-              ),
             ],
+        ),
       ),
       
     );
